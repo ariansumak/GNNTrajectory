18e66b2f8827e0edf74af17fbd8c8f5c3a66dd6c
Arian Sumak <arian.sumak@pufin.id>
2025-11-19 16:40:01 +0100

    fixxed

--- scripts/train_cluster.sh
+++ scripts/train_cluster.sh
@@ -32,11 +32,12 @@
 #BSUB -e train_gnn_traj_%J.err
 # all  BSUB option comments should be above this line!
 
-# execute our command
-if [ ! -d "$HOME/02516_venv" ]; then
-  python3 -m venv "$HOME/02516_venv"
+if [ ! -d "/dtu/blackhole/07/224071/GNNenv" ]; then
+    python3 -m venv "/dtu/blackhole/07/224071/GNNenv"
 fi
-source "$HOME/02516_venv/bin/activate"
+
+# Activate your venv in blackhole
+source "/dtu/blackhole/07/224071/GNNenv/bin/activate"
 module load cuda/12.8.1
 
 cd "$HOME/code/GNNTrajectory"
