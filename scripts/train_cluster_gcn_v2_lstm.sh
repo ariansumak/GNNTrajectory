6a9a5f04f46d586e0e51c50898c81123fdcac6be
Arian Sumak <arian.sumak@pufin.id>
2025-11-23 18:41:54 +0100

    added cluster training scripts

--- scripts/train_cluster_gcn_v2_lstm.sh
+++ scripts/train_cluster_gcn_v2_lstm.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
+### General options
+### -- specify queue --
 #BSUB -q c02516
+### -- Select the resources: 1 gpu in exclusive process mode --
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -J train_gnn_gcn_v2_lstm
+### –- specify queue --
+### -- set the job Name --
+#BSUB -J train_gnn_traj
+### -- ask for number of cores (default: 1) --
 #BSUB -n 4
+### -- specify that the cores must be on the same host --
 #BSUB -R "span[hosts=1]"
+### -- specify that we need xGB of memory per core/slot --
 #BSUB -R "rusage[mem=4GB]"
+###BSUB -R "select[gpu80gb]"
+###BSUB -R "select[gpu32gb]"
+#### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot --
+###BSUB -M 128GB
+### -- set walltime limit: hh:mm --
 #BSUB -W 04:00
+### -- set the email address --
 #BSUB -u s253819@dtu.dk
+### -- send notification at start --
 #BSUB -B
+### -- send notification at completion --
 #BSUB -N
+### -- set the job output file --
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
 #BSUB -o logs/train_gnn_gcn_v2_lstm.out
 #BSUB -e logs/train_gnn_gcn_v2_lstm.err
 
